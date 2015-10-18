96414702d33dc806deef5d189294a342363a0987
MohammedAbuissa <mohammed.abuissa@outlook.com>
2015-10-18 22:03:42 +0200

    Task object added

---
+++ Bubblendar/www/scripts/Task.js
@@ -0,0 +1,16 @@
+﻿function Task()
+    {
+    this.Title =  "Title";
+    this.Description = "Description";
+    this.DeadLine = {
+        Year: (new Date()).getFullYear(),
+        Day: (new Date()).getDate(),
+        Month: (new Date()).getMonth()+1
+    };
+    this.Size =  "1";
+    this.IsScheduled = false;
+    this.Attachments =  new Array();
+    this.AttachURLs = new Array();
+    this.IsDone = false;
+    this.DateCreated = null;
+    }
