5010707e5fd83c2e03c13d5493913f494a3a3f88
Hesham Meneisi <heshammeneisi@gmail.com>
2015-10-27 20:08:46 +0200

    Code moved to file. Still working...

--- Bubblendar/www/scripts/sessionManager.js
+++ Bubblendar/www/scripts/sessionManager.js
@@ -1,6 +1,20 @@
 ﻿function login(username, password) {
-    
+
 };
 function signup(info) {
-    
+    // TODO: Probably not safe this way...
+    Parse.initialize("C5AGX70Viz7bSWdvY7oG05SzkiYUf9CpndqWM58x", "Ab2yzzD3SOQlUMQfDzDvQSwQO4hfdfN8zxjD3jK4");
+    var user = new Parse.User();
+    user.set("username", info.username);
+    user.set("password", info.password);
+    user.set("email", info.email);
+    user.set("nickname", info.nickname)
+
+    user.signUp(null, {
+        success: function (user) {
+        },
+        error: function (user, error) {
+            // TODO: Error handling
+        }
+    });
 };
